92bc2e786a9bd8f522b59c8a248a34c732d5a0a7
brent-cybrid <brent@cybrid.app>
2022-05-24 13:30:56 +0000

    v0.14.5

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/IdentityRecordsBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/IdentityRecordsBankApi.java
@@ -27,7 +27,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2022-05-24T12:58:50.012042Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2022-05-24T13:26:11.536711Z[Etc/UTC]")
 public class IdentityRecordsBankApi {
     private ApiClient apiClient;
 

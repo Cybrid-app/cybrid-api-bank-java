e7e70ebb28f55bfd8dd4ffa9bb1670b75f7ddd73
brent-cybrid <brent@cybrid.app>
2022-06-07 14:46:33 +0000

    v0.20.1

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/SymbolsBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/SymbolsBankApi.java
@@ -24,7 +24,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2022-06-07T13:22:20.131555Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2022-06-07T14:41:57.159390Z[Etc/UTC]")
 public class SymbolsBankApi {
     private ApiClient apiClient;
 

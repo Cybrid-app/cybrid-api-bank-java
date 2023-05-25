bd8af4e246ab9f18f3086b53a40648a5bffa379a
brent-cybrid <brent@cybrid.app>
2023-05-25 22:10:16 +0000

    v0.71.46

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/VerificationKeysBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/VerificationKeysBankApi.java
@@ -28,7 +28,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-05-25T20:08:22.307996Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-05-25T22:05:56.797908Z[Etc/UTC]")
 public class VerificationKeysBankApi {
     private ApiClient apiClient;
 

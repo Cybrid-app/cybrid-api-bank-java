8558697637cbc30bf46e6060e9ce98a7033d13b4
brent-cybrid <brent@cybrid.app>
2023-08-31 16:25:53 +0000

    v0.90.2

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/AssetsBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/AssetsBankApi.java
@@ -25,7 +25,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-31T12:14:49.598525Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-31T16:20:41.334399Z[Etc/UTC]")
 public class AssetsBankApi {
     private ApiClient apiClient;
 
--- src/main/java/app/cybrid/cybrid_api_bank/client/auth/OAuth.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/auth/OAuth.java
@@ -3,7 +3,7 @@ package app.cybrid.cybrid_api_bank.client.auth;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-31T12:14:49.598525Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-31T16:20:41.334399Z[Etc/UTC]")
 public class OAuth implements Authentication {
     private String accessToken;
 

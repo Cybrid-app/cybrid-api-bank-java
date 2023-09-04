61b4dc2c0b90385fc87a51be7e086bc355b8720a
brent-cybrid <brent@cybrid.app>
2023-09-04 21:49:20 +0000

    v0.90.9

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/TradesBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/TradesBankApi.java
@@ -28,7 +28,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-09-04T11:15:02.913756Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-09-04T21:44:05.103157Z[Etc/UTC]")
 public class TradesBankApi {
     private ApiClient apiClient;
 

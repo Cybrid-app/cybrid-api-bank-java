a5ccd10de15db86c092ac2342a266bf9e7a8dbcc
brent-cybrid <brent@cybrid.app>
2023-10-23 14:49:41 +0000

    v0.109.9

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/WorkflowsBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/WorkflowsBankApi.java
@@ -29,7 +29,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-20T11:47:20.255823Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-23T14:44:07.243938Z[Etc/UTC]")
 public class WorkflowsBankApi {
     private ApiClient apiClient;
 

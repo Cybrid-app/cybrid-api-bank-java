de122604fc5d101cf0c62f9039087a736c95f1bf
brent-cybrid <brent@cybrid.app>
2023-10-25 13:07:28 +0000

    v0.109.16

--- src/main/java/app/cybrid/cybrid_api_bank/client/ApiClient.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/ApiClient.java
@@ -69,7 +69,7 @@ import app.cybrid.cybrid_api_bank.client.auth.HttpBearerAuth;
 import app.cybrid.cybrid_api_bank.client.auth.ApiKeyAuth;
 import app.cybrid.cybrid_api_bank.client.auth.OAuth;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-24T12:55:55.715935Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-25T13:02:10.793209Z[Etc/UTC]")
 public class ApiClient extends JavaTimeFormatter {
     public enum CollectionFormat {
         CSV(","), TSV("\t"), SSV(" "), PIPES("|"), MULTI(null);
--- src/main/java/app/cybrid/cybrid_api_bank/client/api/SymbolsBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/SymbolsBankApi.java
@@ -25,7 +25,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-24T12:55:55.715935Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-25T13:02:10.793209Z[Etc/UTC]")
 public class SymbolsBankApi {
     private ApiClient apiClient;
 

ad80d83e30605287a40e07ba88a90bf29e038066
brent-cybrid <brent@cybrid.app>
2023-11-14 20:33:24 +0000

    v0.109.59

--- src/main/java/app/cybrid/cybrid_api_bank/client/auth/HttpBasicAuth.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/auth/HttpBasicAuth.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.Base64Utils;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-14T17:34:40.847451Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-14T20:28:06.548170Z[Etc/UTC]")
 public class HttpBasicAuth implements Authentication {
     private String username;
     private String password;

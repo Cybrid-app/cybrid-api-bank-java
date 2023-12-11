1def6796eb6310691946f1c9dd025034b6a6d04d
brent-cybrid <brent@cybrid.app>
2023-12-11 19:50:45 +0000

    v0.112.30

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/IdentityVerificationsBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/IdentityVerificationsBankApi.java
@@ -29,7 +29,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-12-11T18:56:56.306793Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-12-11T19:45:21.425339Z[Etc/UTC]")
 public class IdentityVerificationsBankApi {
     private ApiClient apiClient;
 

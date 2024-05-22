942f831cd7f5c25ef7d24dfcd3c435b8ef3c2e80
brent-cybrid <brent@cybrid.app>
2024-05-22 17:13:41 +0000

    v0.117.6

--- src/main/java/app/cybrid/cybrid_api_bank/client/auth/HttpBearerAuth.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/auth/HttpBearerAuth.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.Base64Utils;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-05-22T15:22:17.492692Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-05-22T17:05:37.945040Z[Etc/UTC]")
 public class HttpBearerAuth implements Authentication {
     private final String scheme;
     private String bearerToken;

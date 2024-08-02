5b443aed14ddac3a4ebfa74580ae8e5041613367
brent-cybrid <brent@cybrid.app>
2024-08-02 17:23:50 +0000

    v0.118.119

--- src/main/java/app/cybrid/cybrid_api_bank/client/api/PricesBankApi.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/api/PricesBankApi.java
@@ -26,7 +26,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-08-01T18:17:40.810825Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-08-02T17:18:56.369144Z[Etc/UTC]")
 public class PricesBankApi {
     private ApiClient apiClient;
 
--- src/main/java/app/cybrid/cybrid_api_bank/client/auth/HttpBasicAuth.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/auth/HttpBasicAuth.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.Base64Utils;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-08-01T18:17:40.810825Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-08-02T17:18:56.369144Z[Etc/UTC]")
 public class HttpBasicAuth implements Authentication {
     private String username;
     private String password;

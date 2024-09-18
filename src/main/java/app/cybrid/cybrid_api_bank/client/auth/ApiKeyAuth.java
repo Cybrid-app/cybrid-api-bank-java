3233239f7e865ec03824cab483712ea1d6884e2b
brent-cybrid <brent@cybrid.app>
2024-09-18 15:03:10 +0000

    v0.119.111

--- src/main/java/app/cybrid/cybrid_api_bank/client/auth/ApiKeyAuth.java
+++ src/main/java/app/cybrid/cybrid_api_bank/client/auth/ApiKeyAuth.java
@@ -3,7 +3,7 @@ package app.cybrid.cybrid_api_bank.client.auth;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-09-18T14:07:14.371421Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-09-18T14:57:27.849830Z[Etc/UTC]")
 public class ApiKeyAuth implements Authentication {
     private final String location;
     private final String paramName;
